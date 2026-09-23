3ed0797181b52ec9b6ccafdcca00025125830110
G.Calil <gabrielcalil12@hotmail.com>
2026-09-22 23:21:13 -0300

    first commit

---
+++ src/main/java/enigmas_backend/EnigmasBackendApplication.java
@@ -0,0 +1,13 @@
+package enigmas_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnigmasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnigmasBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/enigmas_backend/EnigmasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package enigmas_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnigmasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
